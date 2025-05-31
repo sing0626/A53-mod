9b51c40e4b8db65c63808f5ba984277e1d471c06
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-05-31 14:50:03 +0300

    unica: mods: settings: Disable ASKS
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
    Signed-off-by: Ksawlii <maja.blaszczak@proton.me>

--- unica/mods/settings/customize.sh
+++ unica/mods/settings/customize.sh
@@ -1,2 +1,6 @@
 SET_PROP "system" "ro.unica.version" "$ROM_VERSION"
 SET_PROP "system" "ro.unica.codename" "$ROM_CODENAME"
+
+# ro.build.2ndbrand is always "false"
+LOG "- Disabling ASKS"
+sed -i "s/ro.build.official.release/ro.build.2ndbrand/g" "$APKTOOL_DIR/system/framework/services.jar/smali/com/android/server/asks/ASKSManagerService.smali"
