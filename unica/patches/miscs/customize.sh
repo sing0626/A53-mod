c7813fb35bd0e4cf5ef185f6da8489874db3537c
crok <crok.bic@gmail.com>
2025-06-22 10:02:55 +0200

    unica: patches: misc: import Crok's RAM Managment Fix
    Crok explained what it does here https://github.com/crok/crokrammgmtfix/blob/master/module.prop#L6
    
    Co-authored-by: Ksawlii <maja.blaszczak@proton.me>
    Signed-off-by: Ksawlii <maja.blaszczak@proton.me>

--- unica/patches/miscs/customize.sh
+++ unica/patches/miscs/customize.sh
@@ -29,3 +29,18 @@ fi
 
 # Set custom Display ID prop
 SET_PROP "system" "ro.build.display.id" "$(GET_PROP "system" "ro.build.display.id")-Ksawlii"
+
+# https://github.com/crok/crokrammgmtfix/blob/master/service.sh#L27-L32
+LOG_STEP_IN "- Applying Crok's RAM Managment Fix"
+[ -f "$WORK_DIR/system/system/etc/init/ram_mgmt_fix.rc" ] && rm -f "$WORK_DIR/system/system/etc/init/ram_mgmt_fix.rc"
+{
+    echo "on post-fs-data"
+    echo "    exec_background -- /system/bin/cmd device_config set_sync_disabled_for_tests persistent"
+    echo "    exec_background -- /system/bin/cmd device_config put activity_manager max_cached_processes 256"
+    echo "    exec_background -- /system/bin/cmd device_config put activity_manager max_phantom_processes 2147483647"
+    echo "    exec_background -- /system/bin/cmd settings put global settings_enable_monitor_phantom_procs false"
+    echo "    exec_background -- /system/bin/cmd device_config put activity_manager max_empty_time_millis 43200000"
+    echo "    exec_background -- /system/bin/cmd"
+} >> "$WORK_DIR/system/system/etc/init/ram_mgmt_fix.rc"
+SET_METADATA "system" "system/etc/init/ram_mgmt_fix.rc" 0 0 644 "u:object_r:system_file:s0"
+LOG_STEP_OUT
