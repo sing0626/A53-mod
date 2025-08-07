f59198d5d8a061851ba7418662ff693cacded560
Ksawlii <maja.blaszczak@proton.me>
2025-08-07 15:54:22 +0200

    unica: patches: mass_cam: burp
    Signed-off-by: Ksawlii <maja.blaszczak@proton.me>

--- unica/patches/mass_cam/customize.sh
+++ unica/patches/mass_cam/customize.sh
@@ -1,14 +1,14 @@
 if ! $SOURCE_HAS_MASS_CAMERA_APP; then
-LOG_STEP_IN
     if $TARGET_HAS_MASS_CAMERA_APP; then
+        LOG_STEP_IN "- Adding MASS Camera APP"
         DELETE_FROM_WORK_DIR "system" "system/priv-app/SamsungCamera/oat"
         DELETE_FROM_WORK_DIR "system" "system/priv-app/SamsungCamera/SamsungCamera.apk.prof"
         ADD_TO_WORK_DIR "a73xqxx" "system" "system/app/FunModeSDK/FunModeSDK.apk" 0 0 644 "u:object_r:system_file:s0"
         ADD_TO_WORK_DIR "r11sxxx" "system" "system/priv-app/SamsungCamera/SamsungCamera.apk" 0 0 644 "u:object_r:system_file:s0"
+        LOG_STEP_OUT
     else
-        echo "- TARGET_HAS_MASS_CAMERA_APP is not set. Ignoring"
+        LOG "- TARGET_HAS_MASS_CAMERA_APP is not set. Ignoring"
     fi
 else
-    echo "- SOURCE_HAS_MASS_CAMERA_APP is set. Ignoring"
-LOG_STEP_OUT
+    LOG "- SOURCE_HAS_MASS_CAMERA_APP is set. Ignoring"
 fi
