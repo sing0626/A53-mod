9a34158137ede5d365d781387b828e9e2d751c1c
Ksawlii <maja.blaszczak@proton.me>
2025-08-17 23:00:44 +0200

    unica: mods: paradigm: Hi
    Change-Id: I0585ab06b932d9902ced41441d7bc19efd33fbeb
    Signed-off-by: Ksawlii <maja.blaszczak@proton.me>

--- unica/mods/paradigm/customize.sh
+++ unica/mods/paradigm/customize.sh
@@ -60,7 +60,7 @@ ADD_TO_WORK_DIR "dm1qxx" "system" "system/etc/permissions/privapp-permissions-co
 ADD_TO_WORK_DIR "dm1qxx" "system" "system/etc/sysconfig/preinstalled-packages-com.samsung.android.hwresourceshare.xml" 0 0 644 "u:object_r:system_file:s0"
 LOG_STEP_OUT
 
-LOG_STEP_OUT "- Adding AOD Wallpaper"
+LOG_STEP_IN "- Adding AOD Wallpaper"
 ADD_TO_WORK_DIR "e1qzcx" "system" "system/etc/permissions/privapp-permissions-com.samsung.android.wallpaper.live.xml" 0 0 644 "u:object_r:system_file:s0"
 ADD_TO_WORK_DIR "e1qzcx" "system" "system/priv-app/SpriteWallpaper" 0 0 755 "u:object_r:system_file:s0"
 LOG_STEP_OUT
